04eae6c81662e0fc201c3e63d47d10aab2c2e391
BelenG <90286485+SBelenB@users.noreply.github.com>
2024-01-21 22:15:48 -0300

    Created basic structure and classes, added UserModel

---
+++ crud/src/main/java/com/api/crud/repositories/IUserRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.api.crud.repositories;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface IUserRepository {
+    
+}
