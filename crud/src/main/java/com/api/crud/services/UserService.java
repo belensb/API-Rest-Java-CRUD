da4098ae44fc24277d065b204c8bc02c71aac7fb
BelenG <90286485+SBelenB@users.noreply.github.com>
2024-01-22 03:38:10 -0300

    Added all necessary methods, api working

--- crud/src/main/java/com/api/crud/services/UserService.java
+++ crud/src/main/java/com/api/crud/services/UserService.java
@@ -3,6 +3,7 @@ package com.api.crud.services;
 import com.api.crud.models.UserModel;
 import com.api.crud.repositories.IUserRepository;
 import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,33 @@ public class UserService {
     
     public ArrayList<UserModel> getUsers() {
         return (ArrayList<UserModel>) userRepository.findAll();
+    }
+    
+    public UserModel saveUser(UserModel user){
+        return userRepository.save(user);
+    }
+    
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+    
+    public UserModel updateById(UserModel request, Long id){
+        UserModel user = userRepository.findById(id).get();
+        
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        
+        return user;
+    }
+    
+    public Boolean deleteUser (Long id){
+        try{
+            userRepository.deleteById(id);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
     
 }
