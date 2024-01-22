8413be3224050d2fe763f368d51889af54c0089e
BelenG <90286485+SBelenB@users.noreply.github.com>
2024-01-21 22:25:59 -0300

    Added controller, repository and services

--- crud/src/main/java/com/api/crud/controllers/UserController.java
+++ crud/src/main/java/com/api/crud/controllers/UserController.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.api.crud.controllers;
 
-/**
- *
- * @author Usuario
- */
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+   
+    
     
 }
--- crud/src/main/java/com/api/crud/services/UserService.java
+++ crud/src/main/java/com/api/crud/services/UserService.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.api.crud.services;
 
-/**
- *
- * @author Usuario
- */
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    
+    @Autowired
+    IUserRepository userRepository;
+    
+    public ArrayList<UserModel> getUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
     
 }
